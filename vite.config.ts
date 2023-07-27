43bbb2918eee51eba404a2540f6ea7f49e584835
GershonWang <986432015@qq.com>
2023-07-27 12:15:36 +0800

    更新electron-builder打包

--- vite.config.ts
+++ vite.config.ts
@@ -105,19 +105,6 @@ export default defineConfig(({ command }) => {
         port: +url.port,
       }
     })(),
-    // 本地运行配置，及反向代理配置
-    // server: {
-    //   cors: true, // 默认启用并允许任何源
-    //   //反向代理配置，注意rewrite写法，开始没看文档在这里踩了坑
-    //   proxy: {// 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
-    //     '/api': {
-    //       target: new URL(pkg.debug.env.VITE_DEV_SERVER_URL), // 通过代理接口访问实际地址。这里是实际访问的地址。vue会通过代理服务器来代理请求
-    //       changeOrigin: true,
-    //       ws: true,  // 允许websocket代理
-    //       rewrite: (path) => path.replace(/^\/api/, '') // 将api替换为空
-    //     }
-    //   }
-    // },
     clearScreen: false,
   }
 })
